0b4c0ff7b4d0ce3593871664979e03ee7187e2d8
Matthew Harazim <matthew.harazim.13@aberdeen.ac.uk>
2019-07-28 15:20:47 +0100

    Added windows and Mac builds
    Added a builds folder containing Windows and Mac binaries

---
+++ builds/windows/kyoti_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,102 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRDCHIRP = 2337133864U;
+        static const AkUniqueID BIRDOFPREY = 3885109967U;
+        static const AkUniqueID BIRDSONG = 3886820807U;
+        static const AkUniqueID CAMPFIRE = 1931646578U;
+        static const AkUniqueID CHESTCLOSE = 1044338672U;
+        static const AkUniqueID CHESTOPEN = 2892697348U;
+        static const AkUniqueID CHOPPING = 3243765741U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GUSTS = 1045155115U;
+        static const AkUniqueID JUMPLAND = 1548130050U;
+        static const AkUniqueID JUMPUP = 1844425226U;
+        static const AkUniqueID SKULLDISSONANCE = 2859689529U;
+        static const AkUniqueID STOPCRANE = 2484129336U;
+        static const AkUniqueID UNDERTHESHADOW = 2525710202U;
+        static const AkUniqueID WELL = 1572484541U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace UNDERTHESHADOW
+        {
+            static const AkUniqueID GROUP = 2525710202U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LIGHT = 1935470627U;
+                static const AkUniqueID SHADOW = 3140781661U;
+            } // namespace STATE
+        } // namespace UNDERTHESHADOW
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SAND = 803837735U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID OPENNESS = 4168635916U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID STORMINESS = 2540450804U;
+        static const AkUniqueID TIMEOFDAY = 3729505769U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID EVENTS = 1381315342U;
+        static const AkUniqueID PLAYER = 1069431850U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID WINDBUS = 238088383U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
